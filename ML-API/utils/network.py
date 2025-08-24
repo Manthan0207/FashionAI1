a2bd6176ee3ef2e894f02c752f214a25befeecf0
Manthan-Panseriya <manthan0205@gmail.com>
2025-08-24 21:14:52 +0530

    final push

--- ML-API/utils/network.py
+++ ML-API/utils/network.py
@@ -21,7 +21,6 @@ class REBNCONV(nn.Module):
         return xout
 
 
-## upsample tensor 'src' to have the same spatial size with tensor 'tar'
 def _upsample_like(src, tar):
 
     src = F.upsample(src, size=tar.shape[2:], mode="bilinear")
--- Server/utils/generateTokenAndSetCookie.js
+++ Server/utils/generateTokenAndSetCookie.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config() //so we can access the JWT secret key
 
 export const generateTokenAndSetCookie = (res, userId) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' }) //{userId} is called payload
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
     res.cookie("authToken", token,
         {
             httpOnly: true, // cookie can not be accessed by client side js , prevents XSS attack ,
             secure: process.env.NODE_ENV === 'production',  //it will be https in production while development is http
-            sameSite: "strict", //prevents csrf attack
+            sameSite: "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000
         }
     )
